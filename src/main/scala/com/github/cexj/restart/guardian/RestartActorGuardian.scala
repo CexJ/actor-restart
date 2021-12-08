92b3d4a1982dd4e81a7b833d6c95357787b8df5f
Andrea Cesaro <ace@n-side.com>
2021-12-08 21:20:33 +0100

    restart of actors with guardian

---
+++ src/main/scala/com/github/cexj/restart/guardian/RestartActorGuardian.scala
@@ -0,0 +1,69 @@
+package com.github.cexj.restart.guardian
+
+import akka.actor.ActorSystem
+import akka.actor.typed.{ActorRef, Behavior, SupervisorStrategy, Terminated}
+import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.scaladsl.adapter.ClassicActorSystemOps
+import com.github.cexj.restart.guardian.JobManagerActor.JobManagerActorCommander
+import com.github.cexj.restart.guardian.JobManagerActor.JobManagerActorCommander._
+
+import scala.util.Random
+
+object RestartActorGuardian extends App {
+
+  val system: ActorSystem = ActorSystem("edge")
+
+  val actor = system.spawn(JobManagerGuardian(), Random.nextInt().toString)
+
+  actor ! Print("Hello")
+  actor ! Die
+
+  Thread.sleep(2000)
+
+  actor ! Print("world")
+
+}
+
+object JobManagerGuardian {
+
+  def apply(): Behavior[JobManagerActorCommander] = init()
+  private def init(): Behavior[JobManagerActorCommander] = Behaviors.setup { context =>
+    val actor = context.spawn(JobManagerActor(), "AGAIN")
+    context.watch(actor)
+    watching(actor)
+  }
+
+  private def watching(actor: ActorRef[JobManagerActorCommander]): Behavior[JobManagerActorCommander] =
+    Behaviors.receiveMessage[JobManagerActorCommander]{
+      case msg =>
+        actor ! msg
+        Behaviors.same
+    }.receiveSignal {
+      case (context, Terminated(_)) =>
+        val actor = context.spawn(JobManagerActor(), "AGAIN")
+        context.watch(actor)
+        watching(actor)
+    }
+
+
+}
+
+
+
+object JobManagerActor {
+  trait JobManagerActorCommander
+  object JobManagerActorCommander{
+    case class Print(name: String) extends JobManagerActorCommander
+    case object Die extends JobManagerActorCommander
+  }
+  def apply(): Behavior[JobManagerActorCommander] = init()
+  private def init(): Behavior[JobManagerActorCommander] = Behaviors.setup { context =>
+    Behaviors.receiveMessage[JobManagerActorCommander] {
+      case Print(name) =>
+        println(s"$name")
+        Behaviors.same
+      case Die =>
+        Behaviors.stopped
+    }
+  }
+}
--- src/main/scala/com/github/cexj/restart/wrong/RestartActorWrong.scala
+++ src/main/scala/com/github/cexj/restart/wrong/RestartActorWrong.scala
@@ -4,41 +4,41 @@ import akka.actor.ActorSystem
 import akka.actor.typed.{Behavior, SupervisorStrategy}
 import akka.actor.typed.scaladsl.Behaviors
 import akka.actor.typed.scaladsl.adapter.ClassicActorSystemOps
-import com.github.cexj.restart.wrong.JobManagerActor.JobManagerActorCommander
-import com.github.cexj.restart.wrong.JobManagerActor.JobManagerActorCommander._
+import com.github.cexj.restart.wrong.JobManagerActorW.JobManagerActorCommanderW
+import com.github.cexj.restart.wrong.JobManagerActorW.JobManagerActorCommanderW._
 
 import scala.util.Random
 
 object RestartActorWrong extends App {
 
-  def supervisedJobManagerActor: Behavior[JobManagerActorCommander] = Behaviors.supervise(JobManagerActor()).onFailure(SupervisorStrategy.restart)
+  def supervisedJobManagerActor: Behavior[JobManagerActorCommanderW] = Behaviors.supervise(JobManagerActorW()).onFailure(SupervisorStrategy.restart)
 
   val system: ActorSystem = ActorSystem("edge")
 
   val actor = system.spawn(supervisedJobManagerActor, Random.nextInt().toString)
 
-  actor ! Print("Hello")
-  actor ! Die
+  actor ! PrintW("Hello")
+  actor ! DieW
 
   Thread.sleep(2000)
 
-  actor ! Print("world")
+  actor ! PrintW("world")
 
 }
 
-object JobManagerActor {
-  trait JobManagerActorCommander
-  object JobManagerActorCommander{
-    case class Print(name: String) extends JobManagerActorCommander
-    case object Die extends JobManagerActorCommander
+object JobManagerActorW {
+  trait JobManagerActorCommanderW
+  object JobManagerActorCommanderW{
+    case class PrintW(name: String) extends JobManagerActorCommanderW
+    case object DieW extends JobManagerActorCommanderW
   }
-  def apply(): Behavior[JobManagerActorCommander] = init()
-  private def init(): Behavior[JobManagerActorCommander] = Behaviors.setup { context =>
-    Behaviors.receiveMessage[JobManagerActorCommander] {
-      case Print(name) =>
+  def apply(): Behavior[JobManagerActorCommanderW] = init()
+  private def init(): Behavior[JobManagerActorCommanderW] = Behaviors.setup { context =>
+    Behaviors.receiveMessage[JobManagerActorCommanderW] {
+      case PrintW(name) =>
         println(s"$name")
         Behaviors.same
-      case Die =>
+      case DieW =>
         Behaviors.stopped
     }
   }
